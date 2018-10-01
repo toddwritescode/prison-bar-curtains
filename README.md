50ad4f97097a03cc39bf20d83188d2c38aaf31d7
todd.smyth <Todd.Smyth@leapdev.io>
2018-10-02 09:02:35 +1000

    initial add of code

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # prison-bar-curtains
 Fun little example with an amazing image thanks to William Warby. Check it out here => https://www.flickr.com/photos/wwarby/3256155178/
+Feel free to pull it down and try it for yourself.
---
+++ js/main.js
@@ -0,0 +1,8 @@
+(function(){
+	var pleft = document.getElementById('prisonbarleft');
+	var pright = document.getElementById('prison-bar-right');
+	setTimeout(function(){
+		pleft.classList.add('open');
+		pright.classList.add('open');
+	}, 0);
+})();
